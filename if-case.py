50662759ac3fb73342162e4520b8086a56820f95
Noor Mohammed B <nooru.mohad@gmail.com>
2020-06-21 15:50:42 +0530

    if case

---
+++ if-case.py
@@ -0,0 +1,9 @@
+value = int(input("Enter A Value : "))
+if value <0 :
+    print("The Value is Negetive ")
+elif value > 0 :
+    print("The Value is Positive")
+elif value == 0 :
+    print("The Value is Zero")
+else :
+    print("Invalid")
