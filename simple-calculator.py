8a083d70be77ed21b2b96b9da8fa2d4e3263ef57
Noor Mohammed B <nooru.mohad@gmail.com>
2020-06-21 15:51:35 +0530

    simple calculator

---
+++ simple-calculator.py
@@ -0,0 +1,14 @@
+oprant1 = int(input("Enter First Number For Calculation : "))
+oparator = input("Enter Operator")
+oprant2 = int(input("Enter Second Number For Calculation : "))
+print("Result")
+if oparator == "+" :
+    print(oprant1+oprant2)
+elif oparator == "-" :
+    print(oprant1-oprant2)
+elif oparator == "*" :
+    print(oprant1*oprant2)
+elif oparator == "/" :
+    print(oprant1/oprant2
+else :
+    print("Invalid \n")
