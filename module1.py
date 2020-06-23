5e94faaaee77f79d6c953c2d933f0dffbec8bfbc
Noor Mohammed B <nooru.mohad@gmail.com>
2020-06-23 22:01:45 +0530

    modules differert files

---
+++ module1.py
@@ -0,0 +1,7 @@
+import module2
+
+module2.sum(3,3)
+
+fnc = module2.sum
+
+fnc(4,4)
---
+++ module2.py
@@ -0,0 +1,7 @@
+print("Welcome to ",__name__)
+
+def sum(num1,num2) :
+    print(num1+num2)
+    print("i am from ",__name__," function sum\n")
+
+print("End of ",__name__)
