724898368a855f09163014c03b8a43c6b0ee0249
Noor Mohammed B <nooru.mohad@gmail.com>
2020-06-23 17:24:35 +0530

    function with tuple

--- function.py
+++ function.py
@@ -1,4 +1,7 @@
-print("Hello World")
-def fnc(th) :
-    print(th)
-fnc("what")
+print("Working ")
+
+def function(*value) :
+    print("function with tuple")
+    print(value[0],value[1],value[2])
+
+function("hai ",798657," whatever")
