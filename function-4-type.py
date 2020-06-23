1c463c82fa8450ec6373b9cfd669ce8c7bc078af
Noor Mohammed B <nooru.mohad@gmail.com>
2020-06-23 19:14:00 +0530

    function with no arg and no rtn

---
+++ function-4-type.py
@@ -0,0 +1,8 @@
+# Function with no argument and no return value
+
+print("enter 2 number")
+def main():
+    fnumber = int(input("1. : "))
+    snumber = int(input("2. : "))
+    print("sum = ",fnumber+snumber)
+main()
