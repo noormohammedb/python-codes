4af87c7180b28d6e46e80d1d4b3395dfa8841462
Noor Mohammed B <nooru.mohad@gmail.com>
2020-06-23 17:44:38 +0530

    keyword argument

--- function-operation-with-arguments.py
+++ function-operation-with-arguments.py
@@ -1,6 +1,6 @@
 print("Hello")
 
-def fun(arg1,arg2):
-    print(arg1,arg2)
+def fun(arg1,arg2,arg3):
+    print(arg1,arg2,arg3)
 
-fun("welcome","hai")
+fun(arg2="welcome",arg1="hai",arg3="how are you")
